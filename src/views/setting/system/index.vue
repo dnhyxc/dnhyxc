ce3a87a1acc4d6ec54dab2aa8f2ce21dc9a8fb76
dnhyxc <2640559435@qq.com>
2023-02-22 23:55:42 +0800

    feat：完善系统设置页面

--- src/views/setting/system/index.vue
+++ src/views/setting/system/index.vue
@@ -6,11 +6,22 @@
 -->
 <template>
   <div class="system-wrap">
-    窗口控制
+    <div class="content">
+      <div class="shortcuts">
+        <div class="label">快捷键</div>
+        <div v-for="item in STSTEM_CONFIG.shortcut" :key="item.value" class="item">{{ item.label }}</div>
+      </div>
+      <div class="file-config">
+        <div class="label">文件存储</div>
+        <div v-for="item in STSTEM_CONFIG.fileConfig" :key="item.label" class="item">{{ item.label }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { STSTEM_CONFIG } from '@/constant';
+</script>
 
 <style scoped lang="less">
 @import '@/styles/index.less';
