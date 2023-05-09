3bbb4ae6edd6162e005e95aaac6daccfdf4a10bc
dengnh <dengninghai@shinemo.com>
2023-05-09 11:48:17 +0800

    feat：调整Card图片缩放大小

--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -193,8 +193,8 @@ const toTag = (name: string) => {
 
     &:hover {
       .img {
-        transform: scale(1.3);
-        transition: all 0.3s;
+        transform: scale(1.5);
+        transition: scale 0.5s;
       }
 
       .card-top {
