d92d715deeeb1335215522c45ce443539dd79610
dengnh <dengninghai@shinemo.com>
2023-05-30 20:40:35 +0800

    feat:优化托转上传

--- src/views/tools/Modal/index.vue
+++ src/views/tools/Modal/index.vue
@@ -8,22 +8,29 @@
   <div class="modal-wrap">
     <el-dialog v-model="visible" :close-on-click-modal="false" title="图片压缩" align-center width="600px" draggable>
       <div class="content">
-        <el-upload
-          class="upload"
-          drag
-          multiple
-          :show-file-list="false"
-          :before-upload="beforeUpload"
-          :http-request="onUpload"
-        >
-          <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-          <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-          <template #tip>
-            <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
-          </template>
-        </el-upload>
-        <div ref="imageRef" class="image-container" />
-        <div ref="imageRef" class="berore" />
+        <el-scrollbar ref="scrollRef" wrap-class="scrollbar-wrapper">
+          <el-upload
+            class="upload"
+            drag
+            multiple
+            :show-file-list="false"
+            :before-upload="beforeUpload"
+            :http-request="onUpload"
+          >
+            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+            <div class="el-upload__text">拖拽或点击文件上传</div>
+          </el-upload>
+          <div class="upload-info">仅支持 png、jpg、jpeg、gif 格式的图片</div>
+          <div class="image-container">
+            <div class="before">
+              <span class="info">压缩前</span>
+              <img :src="IMG1" alt="" class="compress-image-origin" />
+            </div>
+            <div ref="imageRef" class="after">
+              <span class="info">压缩后</span>
+            </div>
+          </div>
+        </el-scrollbar>
       </div>
       <template #footer>
         <span class="dialog-footer">
@@ -41,7 +48,7 @@ import { ElMessage } from 'element-plus';
 import { UploadFilled } from '@element-plus/icons-vue';
 import type { UploadProps } from 'element-plus';
 import { uploadStore } from '@/store';
-import { FILE_TYPE } from '@/constant';
+import { FILE_TYPE, IMG1 } from '@/constant';
 import { compressImage } from '@/utils';
 
 interface IProps {
@@ -106,16 +113,58 @@ const onDownload = () => {
 @import '@/styles/index.less';
 
 .modal-wrap {
+  overflow: hidden;
+
+  :deep {
+    .el-dialog__body {
+      padding: 10px;
+    }
+    .el-upload-dragger {
+      padding: 10px 0 20px 0;
+    }
+  }
+
   .content {
+    height: 280px;
+
+    .upload-info {
+      margin-bottom: 10px;
+      color: var(--font-5);
+    }
+
     .image-container {
-      width: 50%;
+      height: 1000px;
+      display: flex;
+      justify-content: flex-start;
+      width: 100%;
       height: auto;
 
-      :deep {
-        .compress-image {
+      .before,
+      .after {
+        width: 25%;
+        padding-right: 20px;
+
+        .compress-image-origin {
           width: 100%;
           height: auto;
+          border-radius: 5px;
           .imgStyle();
+        }
+
+        :deep {
+          .compress-image {
+            width: 100%;
+            height: auto;
+            border-radius: 5px;
+            .imgStyle();
+          }
+        }
+
+        .info {
+          display: inline-block;
+          font-size: 14px;
+          font-weight: 700;
+          margin-bottom: 10px;
         }
       }
     }
