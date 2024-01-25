0eae359523566b9b769f0ec950afc6e77956cd19
dengnh <dengninghai@shinemo.com>
2024-01-25 19:43:31 +0800

    ui(custom):

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -531,7 +531,7 @@ const beauty = {
   '--pre-hover-bg': 'rgb(0, 0, 0, 0.35)',
   // table 偶数项背景颜色
   '--table-even-bg': 'rgba(255, 255, 255, 0.1)',
-  '--placeholder-color': '#686868',
+  '--placeholder-color': '#868686',
   // loading 背景颜色
   '--loading-color': 'rgba(0, 0, 0, 0.69)',
   '--loading-text-color': '#2aff00',
