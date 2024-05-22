b908cc486ff0c30db77a6e603a3b9c89ace7a097
dnhyxc <2640559435@qq.com>
2024-05-22 20:27:56 +0800

    ui(custom): 优化图片集ui

--- src/views/picture/index.vue
+++ src/views/picture/index.vue
@@ -5,11 +5,7 @@
  * index.vue
 -->
 <template>
-  <Loading
-    :key="winSize"
-    :loading="pictureStore.loading"
-    :class="`${checkOS() === 'mac' && 'mac-atlas-wrap'} atlas-wrap`"
-  >
+  <Loading :key="winSize" :loading="pictureStore.loading" class="atlas-wrap">
     <template #default>
       <div class="atlas-header-wrap">
         <div class="upload-btn">
@@ -43,63 +39,65 @@
           </el-button>
         </div>
       </div>
-      <el-scrollbar ref="scrollRef" wrap-class="scrollbar-wrapper">
-        <div
-          v-if="isMounted"
-          v-infinite-scroll="onFetchData"
-          :infinite-scroll-delay="300"
-          :infinite-scroll-disabled="disabled"
-          :infinite-scroll-distance="2"
-          class="pullup-content"
-        >
-          <div v-if="pictureStore.atlasList.length > 0" class="img-list">
-            <div v-for="(item, index) in pictureStore.atlasList" :key="index" class="img-item-wrap">
-              <div class="img-item">
-                <div class="header">
-                  <div class="title" :title="item.fileName" @click="onSelectImages(item)">
-                    <el-checkbox
-                      :key="item.id"
-                      :model-value="selectedImageIds.includes(item.id)"
-                      size="large"
-                      @change="onSelectImages(item)"
-                    />
-                    <span :class="`img-name ${selectedImageIds.includes(item.id) && 'is-checked'}`">
-                      {{ item.fileName }}
-                    </span>
+      <div class="atlas-content">
+        <el-scrollbar ref="scrollRef" wrap-class="scrollbar-wrapper">
+          <div
+            v-if="isMounted"
+            v-infinite-scroll="onFetchData"
+            :infinite-scroll-delay="300"
+            :infinite-scroll-disabled="disabled"
+            :infinite-scroll-distance="2"
+            class="pullup-content"
+          >
+            <div v-if="pictureStore.atlasList.length > 0" class="img-list">
+              <div v-for="(item, index) in pictureStore.atlasList" :key="index" class="img-item-wrap">
+                <div class="img-item">
+                  <div class="header">
+                    <div class="title" :title="item.fileName" @click="onSelectImages(item)">
+                      <el-checkbox
+                        :key="item.id"
+                        :model-value="selectedImageIds.includes(item.id)"
+                        size="large"
+                        @change="onSelectImages(item)"
+                      />
+                      <span :class="`img-name ${selectedImageIds.includes(item.id) && 'is-checked'}`">
+                        {{ item.fileName }}
+                      </span>
+                    </div>
+                    <div class="actions">
+                      <el-dropdown placement="bottom-end" trigger="click" popper-class="custom-dropdown-styles">
+                        <i class="iconfont icon-gengduo3" />
+                        <template #dropdown>
+                          <el-dropdown-menu>
+                            <el-dropdown-item @click="onDownload(item)">下载</el-dropdown-item>
+                            <el-dropdown-item @click="onRename(item)">更名</el-dropdown-item>
+                            <el-dropdown-item @click="onDeleteImage(item)">删除</el-dropdown-item>
+                          </el-dropdown-menu>
+                        </template>
+                      </el-dropdown>
+                    </div>
                   </div>
-                  <div class="actions">
-                    <el-dropdown placement="bottom-end" trigger="click" popper-class="custom-dropdown-styles">
-                      <i class="iconfont icon-gengduo3" />
-                      <template #dropdown>
-                        <el-dropdown-menu>
-                          <el-dropdown-item @click="onDownload(item)">下载</el-dropdown-item>
-                          <el-dropdown-item @click="onRename(item)">更名</el-dropdown-item>
-                          <el-dropdown-item @click="onDeleteImage(item)">删除</el-dropdown-item>
-                        </el-dropdown-menu>
-                      </template>
-                    </el-dropdown>
-                  </div>
+                  <Image
+                    :url="item.url || IMG1"
+                    :transition-img="IMG1"
+                    class="prew-img"
+                    :on-click="() => onPreview(item)"
+                  />
                 </div>
-                <Image
-                  :url="item.url || IMG1"
-                  :transition-img="IMG1"
-                  class="prew-img"
-                  :on-click="() => onPreview(item)"
-                />
               </div>
             </div>
           </div>
-        </div>
-        <div v-if="noMore" class="no-more">没有更多了～～～</div>
-        <Empty v-if="showEmpty" />
-        <ImagePreview v-model:previewVisible="previewVisible" :select-image="selectImage" show-prev-and-next />
-        <RenameModal
-          :key="renameVisible"
-          v-model:renameVisible="renameVisible"
-          :img-id="renameId"
-          :rename-file-name="renameFileName"
-        />
-      </el-scrollbar>
+          <div v-if="noMore" class="no-more">没有更多了～～～</div>
+          <Empty v-if="showEmpty" />
+          <ImagePreview v-model:previewVisible="previewVisible" :select-image="selectImage" show-prev-and-next />
+          <RenameModal
+            :key="renameVisible"
+            v-model:renameVisible="renameVisible"
+            :img-id="renameId"
+            :rename-file-name="renameFileName"
+          />
+        </el-scrollbar>
+      </div>
     </template>
   </Loading>
 </template>
@@ -108,7 +106,7 @@
 import { onMounted, onUnmounted, ref, computed } from 'vue';
 import { useScroller } from '@/hooks';
 import { pictureStore } from '@/store';
-import { scrollTo, debounce, onDownloadFile, Message, checkOS } from '@/utils';
+import { scrollTo, debounce, onDownloadFile, Message } from '@/utils';
 import { AtlasItemParams } from '@/typings/common';
 import { IMG1 } from '@/constant';
 import RenameModal from './RenameModal';
@@ -226,7 +224,7 @@ const onScrollTo = () => {
   justify-content: center;
   box-sizing: border-box;
   height: calc(100% - 8px);
-  padding: 5px 0 11px 0;
+  padding-top: 5px;
   margin: 8px 5px 0 5px;
   border-radius: 5px;
 
@@ -234,11 +232,11 @@ const onScrollTo = () => {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: calc(100% - 42px);
+    width: calc(100% - 41px);
     height: 45px;
-    margin: 5px auto 10px;
+    margin: 5px auto 0;
     padding: 0 10px;
-    box-shadow: 0 0 5px 0 var(--card-shadow);
+    box-shadow: 0 0 3px 0 var(--shadow-mack) inset;
     border-radius: 5px;
 
     .upload-btn {
@@ -285,133 +283,138 @@ const onScrollTo = () => {
     .scrollbar-wrapper();
   }
 
-  .img-list {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    padding: 0 6px;
+  .atlas-content {
+    height: calc(100% - 70px);
+    padding: 10px 0;
 
-    &::after {
-      content: '';
-      flex: auto;
-    }
+    --img-height: 220px;
 
-    .img-item-wrap {
-      width: 25%;
-      height: 220px;
-      padding: 5px;
-      border-radius: 5px;
-      box-sizing: border-box;
-      cursor: pointer;
+    .pullup-content {
+      padding: 0 10px 0;
 
-      .img-item {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        position: relative;
-        width: 100%;
-        height: 100%;
-        border-radius: 5px;
-        padding: 2px 5px 5px;
+      .img-list {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        justify-items: start;
+        grid-gap: 10px;
         box-sizing: border-box;
-        box-shadow: 0 0 5px 0 var(--shadow-mack);
-        background-image: linear-gradient(to top, var(--bg-lg-color1) 0%, var(--bg-lg-color2) 100%);
-        transition: all 0.3s;
 
-        &:hover {
-          box-shadow: 0 0 5px 0 var(--theme-blue);
-        }
+        .img-item-wrap {
+          width: 100%;
+          height: var(--img-height);
+          border-radius: 5px;
+          box-sizing: border-box;
+          cursor: pointer;
 
-        .header {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          height: 32px;
-
-          .title {
+          .img-item {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
             position: relative;
-            flex: 1;
-            display: flex;
-            align-items: center;
-            color: var(--font-1);
-            margin-right: 10px;
-            .ellipsisMore(1);
-
-            .img-name {
-              position: absolute;
-              top: 9px;
-              left: 20px;
-              width: calc(100% - 15px);
-              .ellipsisMore(1);
-            }
-
-            .is-checked {
-              color: var(--theme-blue);
-            }
-          }
-
-          .actions {
-            display: flex;
-            align-items: center;
+            width: 100%;
             height: 100%;
-            margin-right: -5px;
+            border-radius: 5px;
+            padding: 2px 5px 5px;
+            box-sizing: border-box;
+            box-shadow: 0 0 3px 0 var(--shadow-mack) inset;
+            background-image: linear-gradient(to top, var(--bg-lg-color1) 0%, var(--bg-lg-color2) 100%);
+            transition: all 0.3s;
 
             &:hover {
-              .icon-gengduo3 {
-                color: var(--active);
-              }
+              box-shadow: 0 0 3px 0 var(--theme-blue) inset;
             }
 
-            .icon-gengduo3 {
-              color: var(--font-1);
-              font-size: 20px;
-              margin-right: 3px;
-              font-weight: 700;
-            }
+            .header {
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
 
-            .del-btn {
-              text-align: center;
-              color: @font-danger;
-              font-size: 20px;
-              border-radius: 5px;
-              padding: 0 3px;
-              cursor: pointer;
-
-              &:first-child {
-                margin-right: 5px;
-              }
-
-              .icon-shanchu {
-                font-size: 20px;
-              }
-            }
-
-            .download-btn {
-              right: 35px;
-
-              .icon-xiazai1 {
-                font-size: 18px;
+              .title {
+                position: relative;
+                flex: 1;
+                display: flex;
+                align-items: center;
                 color: var(--font-1);
+                margin-right: 10px;
+                .ellipsisMore(1);
+
+                .img-name {
+                  position: absolute;
+                  top: 9px;
+                  left: 20px;
+                  width: calc(100% - 15px);
+                  .ellipsisMore(1);
+                }
+
+                .is-checked {
+                  color: var(--theme-blue);
+                }
+              }
+
+              .actions {
+                display: flex;
+                align-items: center;
+                height: 100%;
+                margin-right: -5px;
+
+                &:hover {
+                  .icon-gengduo3 {
+                    color: var(--active);
+                  }
+                }
+
+                .icon-gengduo3 {
+                  color: var(--font-1);
+                  font-size: 20px;
+                  margin-right: 3px;
+                  font-weight: 700;
+                }
+
+                .del-btn {
+                  text-align: center;
+                  color: @font-danger;
+                  font-size: 20px;
+                  border-radius: 5px;
+                  padding: 0 3px;
+                  cursor: pointer;
+
+                  &:first-child {
+                    margin-right: 5px;
+                  }
+
+                  .icon-shanchu {
+                    font-size: 20px;
+                  }
+                }
+
+                .download-btn {
+                  right: 35px;
+
+                  .icon-xiazai1 {
+                    font-size: 18px;
+                    color: var(--font-1);
+                  }
+                }
               }
             }
-          }
-        }
 
-        .prew-img {
-          display: block;
+            .prew-img {
+              display: block;
 
-          :deep {
-            .image-item {
-              height: calc(202px - 35px);
+              :deep {
+                .image-item {
+                  height: calc(var(--img-height) - 53px);
+                  border-radius: 5px;
+                }
+              }
+            }
+
+            .img {
+              width: 100%;
+              display: block;
               border-radius: 5px;
             }
           }
-        }
-
-        .img {
-          width: 100%;
-          display: block;
-          border-radius: 5px;
         }
       }
     }
@@ -419,23 +422,8 @@ const onScrollTo = () => {
 
   .no-more {
     text-align: center;
-    padding-top: 15px;
-    padding-bottom: 15px;
+    padding: 15px 0 5px;
     color: var(--font-4);
-  }
-}
-
-.mac-atlas-wrap {
-  .img-list {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(242px, 1fr));
-    justify-content: center; /* 水平居中 */
-    padding: 0 6px;
-    // grid-gap: 1px 0; /* 可以设置相应的间距 */
-
-    .img-item-wrap {
-      width: 244px;
-    }
   }
 }
 </style>
